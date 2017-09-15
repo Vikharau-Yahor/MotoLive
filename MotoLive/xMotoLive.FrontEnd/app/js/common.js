7211f0406cca9e75d10ff6a0ad8b524719e8d7f8
DESKTOP-0LQKMJL\Inna <gmccsilv@gmail.com>
2017-09-15 23:22:27 +0300

    correct input count and button buy on helmet page

---
+++ MotoLive/xMotoLive.FrontEnd/app/js/common.js
@@ -0,0 +1,25 @@
+(function ($) {
+    $(document).ready(function () {    	
+
+        var el = $("#count");
+    	$("#count").keydown(function(event) {
+        // Разрешаем: backspace, delete, tab и escape
+        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || 
+             // Разрешаем: Ctrl+A
+            (event.keyCode == 65 && event.ctrlKey === true) || 
+             // Разрешаем: home, end, влево, вправо
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+                 // Ничего не делаем
+                 return;
+        }
+        else {
+            // Убеждаемся, что это цифра, и останавливаем событие keypress
+            if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault(); 
+            }   
+        }
+    });
+  
+
+    });
+})(jQuery);
